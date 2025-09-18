cf2ad38e07c5788ce0f7dc3377bfbdece407cec5
klitn <klinhtruong04@gmail.com>
2025-09-18 12:06:12 +0700

    EditManageSchedule

--- production/Healcare-client-ReactJS/src/containers/System/Doctor/ManageSchedule.js
+++ production/Healcare-client-ReactJS/src/containers/System/Doctor/ManageSchedule.js
@@ -1,277 +1,267 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
-// import Header from "../containers/Header/Header";
-import "./ManageSchedule.scss";
+import React, { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import { FormattedMessage } from "react-intl";
-import * as actions from "../../../store/actions";
-import Select from "react-select";
-import { LANGUAGES, dateFormat } from "../../../utils";
-// import DatePicker from "../../../components/Input/DatePicker";
-import DatePicker from "react-date-picker";
-import "react-date-picker/dist/DatePicker.css";
-import "react-calendar/dist/Calendar.css";
-import moment from "moment";
 import { toast } from "react-toastify";
+import moment from "moment";
 import _ from "lodash";
+
+// Components
+import Select from "react-select";
+
+// Actions and Services
+import * as actions from "../../../store/actions";
+import { LANGUAGES, dateFormat } from "../../../utils";
 import { saveBulkScheduleDoctor } from "../../../services/userService";
-class ManageSchedule extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      selectedDoctor: "",
-      listDoctors: "",
-      currDate: "",
-      rangeTime: [],
-      textDate: "",
-      isDatePickerOpen: false,
+
+// Styles
+import "./ManageSchedule.scss";
+
+const ManageSchedule = () => {
+  const dispatch = useDispatch();
+  
+  // Redux state
+  const { language, allDoctors, allScheduleTime } = useSelector(state => ({
+    language: state.app.language,
+    allDoctors: state.admin.allDoctors,
+    allScheduleTime: state.admin.allScheduleTime
+  }));
+
+  // Local state
+  const [selectedDoctor, setSelectedDoctor] = useState(null);
+  const [doctorOptions, setDoctorOptions] = useState([]);
+  const [selectedDate, setSelectedDate] = useState(null);
+  const [timeSlots, setTimeSlots] = useState([]);
+  const [displayDate, setDisplayDate] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  // Build doctor options for select
+  const buildDoctorOptions = (doctors) => {
+    if (!doctors || doctors.length === 0) return [];
+    
+    return doctors.map((doctor) => {
+      const labelVi = `${doctor.lastName} ${doctor.firstName}`;
+      const labelEn = `${doctor.firstName} ${doctor.lastName}`;
+      return {
+        value: doctor.id,
+        label: language === LANGUAGES.VI ? labelVi : labelEn
+      };
+    });
+  };
+
+  // Format date for display
+  const formatDateDisplay = (date) => {
+    if (!date) return "";
+    
+    const options = {
+      year: "numeric",
+      month: "2-digit", 
+      day: "2-digit",
+      weekday: "long"
     };
-  }
 
-  buildDataSelectDoctor = (inputData) => {
-    let results = [];
-    let language = this.props.language;
-    // console.log(language);
-    if (inputData && inputData.length > 0) {
-      inputData.map((item, index) => {
-        let object = {};
-        let labelVi = `${item.lastName} ${item.firstName}`;
-        let labelEn = `${item.firstName} ${item.lastName}`;
-        object.label = language === LANGUAGES.VI ? labelVi : labelEn;
-        object.value = item.id;
-        results.push(object);
-      });
-    }
-
-    return results;
+    return language === LANGUAGES.VI 
+      ? date.toLocaleDateString("vi-VN", options)
+      : date.toLocaleDateString("en-US", options);
   };
 
-  componentDidMount() {
-    this.props.fetchAllDoctors();
-    this.props.fetchAllScheduleTime();
-  }
+  // Initialize data on component mount
+  useEffect(() => {
+    dispatch(actions.fetchAllDoctors());
+    dispatch(actions.fetchAllScheduleTime());
+  }, [dispatch]);
 
-  componentDidUpdate(prevProps) {
-    // console.log("componentDidUpdate", prevProps.allDoctors);
-    if (prevProps.allDoctors !== this.props.allDoctors) {
-      let dataSelect = this.buildDataSelectDoctor(this.props.allDoctors);
-      this.setState({
-        listDoctors: dataSelect,
-      });
-    }
+  // Update doctor options when allDoctors or language changes
+  useEffect(() => {
+    const options = buildDoctorOptions(allDoctors);
+    setDoctorOptions(options);
+  }, [allDoctors, language]);
 
-    if (prevProps.language !== this.props.language) {
-      let dataSelect = this.buildDataSelectDoctor(this.props.allDoctors);
-      this.setState({
-        listDoctors: dataSelect,
-      });
+  // Update time slots when allScheduleTime changes
+  useEffect(() => {
+    if (allScheduleTime && allScheduleTime.length > 0) {
+      const slots = allScheduleTime.map((time) => ({
+        ...time,
+        isSelected: false
+      }));
+      setTimeSlots(slots);
     }
-    if (prevProps.allScheduleTime !== this.props.allScheduleTime) {
-      let data = this.props.allScheduleTime;
-      if (data && data.length > 0) {
-        data = data.map((item) => {
-          item.isSelected = false;
-          return item;
-        });
-      }
-      this.setState({
-        rangeTime: this.props.allScheduleTime,
-      });
-    }
-  }
+  }, [allScheduleTime]);
 
-  handleChangeSelect = async (selectedOption) => {
-    this.setState({ selectedDoctor: selectedOption });
+  // Handle doctor selection
+  const handleDoctorChange = (selectedOption) => {
+    setSelectedDoctor(selectedOption);
   };
-  handleClickBtnTime = (time) => {
-    let { rangeTime } = this.state;
-    let rangeTimeSelected = [];
-    if (rangeTime && rangeTime.length > 0) {
-      rangeTimeSelected = rangeTime.map((item) => {
-        if (item.id === time.id) {
-          item.isSelected = !item.isSelected;
-        }
-        return item;
-      });
-      // console.log("selected", rangeTimeSelected);
-      this.setState({ rangeTime: rangeTimeSelected });
+
+  // Handle date selection
+  const handleDateChange = (date) => {
+    setSelectedDate(date);
+    if (date) {
+      setDisplayDate(formatDateDisplay(new Date(date)));
+    } else {
+      setDisplayDate("");
     }
   };
 
-  handleSaveSchedule = async () => {
-    let { rangeTime, selectedDoctor, currDate } = this.state;
-    let result = [];
-    // console.log("curr range time: ", rangeTime);
-    if (!selectedDoctor && _.isEmpty(selectedDoctor)) {
+  // Handle time slot selection
+  const handleTimeSlotClick = (timeSlot) => {
+    const updatedSlots = timeSlots.map((slot) =>
+      slot.id === timeSlot.id 
+        ? { ...slot, isSelected: !slot.isSelected }
+        : slot
+    );
+    setTimeSlots(updatedSlots);
+  };
+
+  // Handle save schedule
+  const handleSaveSchedule = async () => {
+    // Validation
+    if (!selectedDoctor || _.isEmpty(selectedDoctor)) {
       toast.error(<FormattedMessage id="manage-schedules.invalidDoctor" />);
       return;
     }
-    if (!currDate) {
+
+    if (!selectedDate) {
       toast.error(<FormattedMessage id="manage-schedules.invalidDate" />);
       return;
     }
-    // console.log("currDate: " + currDate);
-    // let formatedDate = moment(currDate).unix();
-    let formatedDate = new Date(currDate).getTime();
-    // console.log("format date", formatedDate);
 
-    if (rangeTime && rangeTime.length > 0) {
-      let selectedTime = rangeTime.filter((item) => item.isSelected === true);
+    const selectedTimeSlots = timeSlots.filter((slot) => slot.isSelected);
+    if (selectedTimeSlots.length === 0) {
+      toast.error("Vui lòng chọn ít nhất một khung giờ");
+      return;
+    }
 
-      if (selectedTime && selectedTime.length > 0) {
-        selectedTime.map((schedule) => {
-          let object = {};
-          object.doctorId = selectedDoctor.value;
-          object.date = formatedDate;
-          object.timeType = schedule.keyMap;
-          result.push(object);
-        });
+    setIsLoading(true);
+    try {
+      // Format date to timestamp
+      const formattedDate = new Date(selectedDate).getTime();
+      
+      // Build schedule array
+      const scheduleArray = selectedTimeSlots.map((timeSlot) => ({
+        doctorId: selectedDoctor.value,
+        date: formattedDate,
+        timeType: timeSlot.keyMap
+      }));
+
+      // Save schedule
+      const response = await saveBulkScheduleDoctor({
+        arraySchedule: scheduleArray,
+        doctorId: selectedDoctor.value,
+        formatedDate: formattedDate
+      });
+
+      if (response.errCode === 0) {
+        toast.success(<FormattedMessage id="manage-schedules.saveChangesSuccess" />);
+        // Reset selected time slots
+        setTimeSlots(prev => prev.map(slot => ({ ...slot, isSelected: false })));
       } else {
-        console.log("invalid selected time");
-        return;
+        toast.error(<FormattedMessage id="manage-schedules.saveChangesFailed" />);
       }
-    }
-    // console.log("selectedTime", result);
-    // console.log("check result", result);
-    let res = await saveBulkScheduleDoctor({
-      arraySchedule: result,
-      doctorId: selectedDoctor.value,
-      formatedDate: formatedDate,
-    });
-    if (res.errCode === 0) {
-      toast.success(
-        <FormattedMessage id="manage-schedules.saveChangesSuccess" />
-      );
-    } else {
+    } catch (error) {
+      console.error("Error saving schedule:", error);
       toast.error(<FormattedMessage id="manage-schedules.saveChangesFailed" />);
+    } finally {
+      setIsLoading(false);
     }
   };
 
-  handleOnChangeDatePicker = (date) => {
-    let formattedDate;
-    const options = {
-      year: "numeric",
-      month: "2-digit",
-      day: "2-digit",
-      weekday: "long", // Include the full name of the day of the week
-    };
+  return (
+    <div className="manage-schedule-wrapper">
+      <div className="manage-schedule-container">
+        <div className="admin-header">
+          <div className="admin-title">
+            <h1>
+              <FormattedMessage id="manage-schedules.title" />
+            </h1>
+          </div>
+        </div>
 
-    if (this.props.language === LANGUAGES.VI) {
-      // For Vietnamese language, format as "Thứ N, dd/mm/yyyy"
-      formattedDate = date.toLocaleDateString("vi-VN", options);
-    } else {
-      // For English language, format as "Monday, mm/dd/yyyy"
-      formattedDate = date.toLocaleDateString("en-US", options);
-    }
-
-    this.setState({
-      currDate: date,
-      textDate: formattedDate,
-    });
-  };
-
-  handleDatePickerClick = () => {
-    this.setState((prevState) => ({
-      isDatePickerOpen: !prevState.isDatePickerOpen,
-    }));
-  };
-  render() {
-    const { selectedDoctor, listDoctors } = this.state;
-    // console.log(listDoctors);
-    let { language } = this.props;
-    // console.log(this.state);
-    let rangeTime = this.state.rangeTime;
-    // console.log("range time", rangeTime);
-    return (
-      <>
-        <div className="container manage-schedule-doctor">
-          <span className="title d-block">
-            <FormattedMessage id="manage-schedules.title" />
-          </span>
-          <div className="select-container row mb-3 mt-3">
-            <div className="col-md-6 mb-3">
-              <label>
+        <div className="schedule-form">
+          {/* Doctor and Date Selection */}
+          <div className="form-row">
+            {/* Doctor Selection */}
+            <div className="form-group">
+              <label className="form-label">
                 <FormattedMessage id="manage-schedules.pick-doctor" />
               </label>
-              <div className="input-area">
-                <Select
-                  value={selectedDoctor}
-                  onChange={this.handleChangeSelect}
-                  options={listDoctors}
-                />
-              </div>
+              <Select
+                value={selectedDoctor}
+                onChange={handleDoctorChange}
+                options={doctorOptions}
+                placeholder="Chọn bác sỹ..."
+                className="doctor-select"
+                classNamePrefix="select"
+                isClearable
+              />
             </div>
-            <div className="input-area col-md-6">
-              <label>
+
+            {/* Date Selection */}
+            <div className="form-group">
+              <label className="form-label">
                 <FormattedMessage id="manage-schedules.pick-date" />
               </label>
-
-              <div className="date-picker" onClick={this.handleDatePickerClick}>
-                <DatePicker
-                  onChange={this.handleOnChangeDatePicker}
-                  className="form-control date-picker-select"
-                  selected={this.state.currDate}
-                  minDate={new Date(new Date().getTime() - 24 * 60 * 60 * 1000)}
-                  // minDate={new Date()}
-                  style={{ fontSize: "18px" }}
-                  format={dateFormat.SEND_TO_SERVER}
-                  isOpen={this.state.isDatePickerOpen}
-                />
+              <div className="date-picker-wrapper">
                 <input
-                  type="text"
-                  className="date-picker-text"
-                  value={this.state.textDate}
-                  readOnly // Make the input read-only to display the selected date
-                  placeholder="Select a date"
+                  type="date"
+                  value={selectedDate ? new Date(selectedDate).toISOString().split('T')[0] : ''}
+                  onChange={(e) => handleDateChange(e.target.value)}
+                  className="date-picker-field"
+                  min={new Date().toISOString().split('T')[0]}
                 />
+                <div className="calendar-icon">
+                  <i className="fas fa-calendar-alt"></i>
+                </div>
+                {displayDate && (
+                  <div className="date-display">{displayDate}</div>
+                )}
               </div>
             </div>
           </div>
-          <div className=" pick-time-container">
-            {rangeTime &&
-              rangeTime.length > 0 &&
-              rangeTime.map((item, index) => {
-                // console.log("item", item);
-                return (
-                  <button
-                    className={
-                      item.isSelected === true
-                        ? "btn btn-custom active px-5 border mx-2 mt-2  d-block"
-                        : "btn btn-custom px-5 border mx-2 mt-2  d-block"
-                    }
-                    key={index}
-                    onClick={() => this.handleClickBtnTime(item)}
-                  >
-                    {language === LANGUAGES.VI ? item.valueVI : item.valueEN}
-                  </button>
-                );
-              })}
+
+          {/* Time Slots Selection */}
+          <div className="time-slots-section">
+            <h3 className="section-title">
+              Chọn khung giờ khám
+            </h3>
+            <div className="time-slots-grid">
+              {timeSlots.map((timeSlot, index) => (
+                <button
+                  key={index}
+                  type="button"
+                  className={`time-slot-btn ${timeSlot.isSelected ? 'selected' : ''}`}
+                  onClick={() => handleTimeSlotClick(timeSlot)}
+                >
+                  {language === LANGUAGES.VI ? timeSlot.valueVI : timeSlot.valueEN}
+                </button>
+              ))}
+            </div>
           </div>
-          <button
-            className="btn btn-primary px-5 mt-3 d-block"
-            onClick={this.handleSaveSchedule}
-          >
-            <FormattedMessage id="manage-schedules.btnSave" />
-          </button>
+
+          {/* Submit Button */}
+          <div className="form-actions">
+            <button
+              type="button"
+              className="btn-save"
+              onClick={handleSaveSchedule}
+              disabled={isLoading}
+            >
+              {isLoading ? (
+                <>
+                  <i className="fas fa-spinner fa-spin"></i>
+                  Đang lưu...
+                </>
+              ) : (
+                <>
+                  <i className="fas fa-save"></i>
+                  <FormattedMessage id="manage-schedules.btnSave" />
+                </>
+              )}
+            </button>
+          </div>
         </div>
-      </>
-    );
-  }
-}
-
-const mapStateToProps = (state) => {
-  return {
-    isLoggedIn: state.user.isLoggedIn,
-    allDoctors: state.admin.allDoctors,
-    language: state.app.language,
-    allScheduleTime: state.admin.allScheduleTime,
-  };
+      </div>
+    </div>
+  );
 };
 
-const mapDispatchToProps = (dispatch) => {
-  return {
-    fetchAllDoctors: () => dispatch(actions.fetchAllDoctors()),
-    fetchAllScheduleTime: () => dispatch(actions.fetchAllScheduleTime()),
-  };
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(ManageSchedule);
+export default ManageSchedule;
