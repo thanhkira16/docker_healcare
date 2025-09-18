a1e900fbbe54aebac7d1c37e5fb70a330663a296
klitn <klinhtruong04@gmail.com>
2025-09-18 10:09:54 +0700

    editclinic

--- production/Healcare-client-ReactJS/src/containers/System/Clinic/AddClinicModal.js
+++ production/Healcare-client-ReactJS/src/containers/System/Clinic/AddClinicModal.js
@@ -1,4 +1,4 @@
-gitimport React, { useState } from "react";
+import React, { useState } from "react";
 import { FormattedMessage, useIntl } from "react-intl";
 import MarkdownIt from "markdown-it";
 import MdEditor from "react-markdown-editor-lite";
