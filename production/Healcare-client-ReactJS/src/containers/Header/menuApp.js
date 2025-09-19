c2090673cce78b2589f8efec68eac7a3592b399b
thanhkira16 <hthanh16092004@gmail.com>
2025-09-19 13:09:00 +0700

    final

--- production/Healcare-client-ReactJS/src/containers/Header/menuApp.js
+++ production/Healcare-client-ReactJS/src/containers/Header/menuApp.js
@@ -3,20 +3,20 @@ export const adminMenu = [
     //Manage user
     name: "menu.admin.manage-user",
     menus: [
-      {
-        name: "menu.admin.crud",
-        link: "/system/user-manage",
-        // subMenus: [
-        //   {
-        //     name: "menu.system.system-administrator.user-manage",
-        //     link: "/system/user-manage",
-        //   },
-        //   {
-        //     name: "menu.system.system-administrator.user-redux",
-        //     link: "/system/user-redux",
-        //   },
-        // ],
-      },
+      // {
+      //   name: "menu.admin.crud",
+      //   link: "/system/user-manage",
+      //   subMenus: [
+      //     {
+      //       name: "menu.system.system-administrator.user-manage",
+      //       link: "/system/user-manage",
+      //     },
+      //     {
+      //       name: "menu.system.system-administrator.user-redux",
+      //       link: "/system/user-redux",
+      //     },
+      //   ],
+      // },
       // { name: "menu.admin.crud-redux", link: "/system/user-redux" },
       { name: "menu.admin.manage-admin", link: "/system/user-admin" },
       { name: "menu.admin.manage-doctor", link: "/system/manage-doctor" },
@@ -44,12 +44,12 @@ export const adminMenu = [
       { name: "menu.admin.manage-specialty", link: "/system/manage-specialty" },
     ],
   },
-  {
-    name: "menu.admin.handbook",
-    menus: [
-      { name: "menu.admin.manage-handbook", link: "/system/manage-handbook" },
-    ],
-  },
+  // {
+  //   name: "menu.admin.handbook",
+  //   menus: [
+  //     { name: "menu.admin.manage-handbook", link: "/system/manage-handbook" },
+  //   ],
+  // },
 ];
 
 export const doctorMenu = [
